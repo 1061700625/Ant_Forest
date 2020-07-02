728daaaf1a143e706f1a4c872d618f11ff46020b
SXF <1061700625@qq.com>
2020-07-02 00:05:08 -0500

    添加了点注释

--- 蚂蚁森林自动收能量.js
+++ 蚂蚁森林自动收能量.js
@@ -1,7 +1,8 @@
 var MODE=0;
 
 
-function CloseApp() {
+function CloseApp() 
+{
     let packageName = currentPackage();
     app.openAppSetting(packageName);
     text(app.getAppName(packageName)).waitFor();  
@@ -21,77 +22,39 @@ function CloseApp() {
 function EnterApp()
 {
     toast("启动支付宝")
-    app.launchApp("支付宝");
-    //text("蚂蚁森林").waitFor();
+    app.launchPackage("com.eg.android.AlipayGphone");
     toast("启动完成");
     sleep(1000);
-    
     className("android.widget.TextView").text("付钱").waitFor();
-    //bounds(33,103,843,180).click();
     var res = className("android.widget.TextView").text("蚂蚁森林").findOne();
     press(res.bounds().centerX(), res.bounds().centerY(), 80);
-    
-    /*
-    className("android.widget.ViewFlipper").click();
-    sleep(1000);
-    className("android.widget.TextView").desc("搜索").waitFor();
-    //bounds(231,76,905,207).setText("蚂蚁森林");
-    className("android.widget.EditText").setText("蚂蚁森林");
-    sleep(500);
-    className("android.widget.FrameLayout").desc("搜索").click();
-    className("android.widget.TextView").text("蚂蚁森林，为你在荒漠种下一棵真树").waitFor();
-    sleep(1000);
-    //bounds(44,757,291,996).click();
-    className("android.widget.FrameLayout").clickable(true).depth(13).indexInParent(0).drawingOrder(1).click();
-    
-    toast("进入森林中...");
-    var cnt = 0;
-    while(cnt++<5)
-    {
-        sleep(500);
-        if(className("android.view.View").text("社区生活 - 生活号").exists())
-        {
-            back();
-            break;
-        }
-    }*/
     className("android.widget.Button").text("种树").waitFor();
     toast("进入完成");
 }
 
-function RunApp(){
+function RunApp()
+{
     EnterApp();
       while(true){
         var dates = new Date();
         var min = dates.getMinutes();
-        if(dates.getHours() == startTime[0] && min >= startTime[1] && min < endTime[1])
-        {
+        if(dates.getHours() == startTime[0] && min >= startTime[1] && min < endTime[1]){
             MyPower();
         }
-        else if(dates.getHours() == endTime[0] && min >= endTime[1])
-        {
+        else if(dates.getHours() == endTime[0] && min >= endTime[1]){
             toast("结束");
-            //exit();
             break;
         }
-        else
-        {
+        else{
             toast("未到时间");
             sleep(5000);
         }
-        /*
-        var res = MyPower();
-        if(res)
-            break;
-        else{
-            sleep(500);
-        }
-        */
     }
 }
 
 
-function getIcon(){
+function getIcon()
+{
     if(!files.exists("icon.jpg")){
         var url="http://121.36.68.53/WEB/SHARE/icon.jpg";
         var res = http.get(url);
@@ -106,36 +69,14 @@ function getIcon(){
 }
 
 var MP_valid = 0;
-function MyPower(){
+function MyPower()
+{
     var cnt = 0;
     MP_valid = 0;
-    /*
-    var p=true;
-    while(p){
-        toast("找一找");
-        var img = captureScreen();
-        while(!img)
-        {
-          sleep(100);
-          img = captureScreen();
-        }
-        toast("截图完成!");
-
-        p = images.findMultiColors(img, "#57b96c", [[17, 0, "#31a63e"], [48, 0, "#3aa44a"], [97, 0, "#ffffff"]], {threshold:15});
-        if(p) {
-          toast(p);
-          click(p.x,p.y-20);
-          MP_valid += 1;
-          sleep(500);
-        }
-        else toast("没找到");
-        if((MODE==4||MODE==2||MODE==1) && cnt++>=5) return 1;
-    } */
-    
     while (true){
         var powerList = className("android.widget.Button").textStartsWith("收集能量").find();
-        if(!powerList.empty()){
-            powerList.forEach(function(item){
+        if(!powerList.empty()){                 // 有能量球
+            powerList.forEach(function(item){   // 依次点击
                 press(item.bounds().centerX(), item.bounds().centerY(), 80);
                 toast("收取一次");
                 MP_valid += 1;
@@ -145,16 +86,15 @@ function MyPower(){
         }else{
             toast("没找到");
         }
-        if((MODE==4||MODE==2||MODE==1) && cnt++>=5) return 1;
+        if(cnt++>=5){  // 不管，5轮后退出
+            return -1;
+        }
     }
-
-    return MP_valid;
 }
 
 
 function findImg(path)
 {
-    //toast("截图中...");
     var img = captureScreen();
     while(!img)
     {
@@ -173,95 +113,70 @@ function findImg(path)
 }
 
 
-function FriendPower(){
-    if(MODE==4 || (MODE==1&&tempM1==1))
-    {
+function FriendPower()
+{
+    if(MODE==4 || (MODE==1&&tempM1==1)){        // 进入蚂蚁森林
         EnterApp();    
     }
     
-    className("android.view.View").text("查看更多好友").findOne().click();
+    className("android.view.View").text("查看更多好友").findOne().click();  //进入好友列表界面
     
-    if(MODE==4 || MODE==2 || MODE==1)
-    {
+    if(MODE==4 || MODE==2 || MODE==1){          // 适用：1马上收好友(无限循环)、2定时收自己再收好友、4马上收好友
         sleep(1000);
         toast("好友模式");
-        tempM1 = 0;
-        var skip_cnt = 0;
+        tempM1 = 0;                             // 下次执行本函数，不需要再进入蚂蚁森林
         var icon = images.read("icon.jpg");
         var img_last = null;
-        while(true)
-        {
-            // var p = findImg();
-            var img = captureScreen();
-            while(!img)
-            {
+        while(true){
+            var img = captureScreen();          // 截图
+            while(!img){
                 sleep(100);
                 img = captureScreen();
             }
             toast("截图完成!");
-            var p = findImage(img, icon);
-            if(p)
-            {
+            var p = findImage(img, icon);       // 找小手图片
+            if(p){                              // 找到
                 toast("找到");
-                click(p.x-500, p.y+50);       
+                click(p.x-500, p.y+50);         // 点击进入
                 sleep(2000);     
-                while(true)
-                {
-                    if(className("android.view.View").text("TA收取你").exists())
-                    //if(className("android.widget.Button").text("浇水").exists())
-                    {
-                        MyPower();
-                       // while(!className("android.view.View").text("总排行榜").exists()){
-                            back();
-                            sleep(1000);
-                       // }
+                while(true){
+                    if(className("android.view.View").text("TA收取你").exists()){ // 等待进入完成
+                        MyPower();              // 收能量
+                        sleep(500);
+                        back();                 // 返回至好友列表界面
+                        sleep(500);
                         break;
                     }
                     sleep(500);
                 }
              }
-            else
-            {
-                if(img_last && findImage(img, img_last)){//两次相等
-                    if(MODE==1){
+            else{                                           // 未找到小手图片
+                if(img_last && findImage(img, img_last)){   // 前后两次截图相等（说明到底部）
+                    if(MODE==1){                            // 适用：1马上收好友(无限循环)
                         toast("重复一遍！");
-                        while(!className("android.view.View").text("查看更多好友").exists())
-                        {
-                            back();
-                            sleep(500);
+                        while(!className("android.view.View").text("查看更多好友").exists()){
+                            back();                         // 返回到上一级
+                            sleep(1000);
                         }
-                        return;
-                    }else{
+                        return;                             // 退出本函数
+                    }else{                                  // 适用：2定时收自己再收好友、4马上收好友
                         toast("结束退出！");
-                        exit();
+                        exit();                             // 结束脚本程序
                     }
                 }
-                img_last = images.copy(img);
+                img_last = images.copy(img);                // 保存本次截图内容
                 toast("上滑");
-                swipe(500,500,500,800,100);
+                swipe(500,500,500,800,500);                 // 先下滑，以便加载更多好友
                 sleep(500);
-                swipe(500,1800,500,500,500);
-                
-                
-                // skip_cnt += 1;
-                // if(skip_cnt > 10)
-                // {
-                //     toast("结束退出！");
-                //     /*while(!className("android.widget.TextView").text("首页").exists())
-                //     {
-                //         back();
-                //         sleep(500);
-                //     }
-                //     return;*/
-                //     exit();
-                // }
+                swipe(500,1800,500,500,500);                // 上滑
             }
             sleep(500);
         }
     }
 }
 
-function main(){    
+function main()
+{    
   device.wakeUp();
   sleep(500);
   device.wakeUpIfNeeded();
@@ -271,15 +186,15 @@ function main(){
   sleep(1000);
   gesture(1000, [540,1425], [250,1700],[560,1700],[540,2020],[840,1700]);
   sleep(1000);
-  RunApp();
-  if(MODE==2)
-  {
+  RunApp();             // 进入app收自己
+  if(MODE==2){          // 适用：2定时收自己再收好友
       FriendPower();
   }
-  exit();
+  exit();                // 退出脚本程序
 }
 
-function selectMode(){
+function selectMode()
+{
     var options = ["定时收自己", "马上收好友(无限循环)", "定时收自己再收好友", "马上收自己","马上收好友"]
     var i = dialogs.select("请选择一个选项(默认第一项)", options);
     if(i >= 0){
@@ -294,9 +209,8 @@ function selectMode(){
 
 //程序从这里开始
 auto();
-alert("使用须知", "需开启无障碍服务、通知栏权限，音量下键可中止脚本(当前关闭，自助开启)；本程序监听状态栏自动启动，支持自动亮屏解锁，由蚂蚁森林通知触发收能量。");
+alert("使用须知", "需开启无障碍服务、通知栏权限，音量下键可中止脚本；本程序监听状态栏自动启动，自动亮屏解锁，由蚂蚁森林通知触发收能量。");
 alert("注意", "初次使用，程序中main()中的gesture()函数，是解锁功能，需要修改成自己的锁屏手势。可通过打开开发者选项中的指针位置查看坐标。");
-//setScreenMetrics(1080, 2340);
 if(!requestScreenCapture()){
     toast("请求截图失败");
     exit();
@@ -305,9 +219,7 @@ getIcon();
 
 selectMode();
 sleep(500);
-if(MODE==1 || MODE==4)
-{
-    //alert("注意！请核对'小手'图片存放位置，可在findImg()函数中修改，如果图片跟脚本是同一个目录，就不用改。默认: icon.jpg");
+if(MODE==1 || MODE==4){
     tempM1 = 1;
     while(true){
         FriendPower();
@@ -327,8 +239,7 @@ sleep(1000);
 console.hide();
 
 toast("开始");
-if(MODE==3)
-{
+if(MODE==3){
     RunApp();    
     exit();
 }
@@ -337,8 +248,7 @@ if(MODE==3)
 events.observeNotification();
 events.on("notification", function(n){
     log("收到新通知:\n 标题: %s, 内容: %s, \n包名: %s", n.getTitle(), n.getText(), n.getPackageName());
-    //if(n.getPackageName()=="com.eg.android.AlipayGphone")
-    {
+    if(n.getPackageName()=="com.eg.android.AlipayGphone"){
       if(n.getTitle()=="你的能量快成熟了")
         main();
     }
@@ -346,15 +256,14 @@ events.on("notification", function(n){
 
 
 threads.start(function(){
-    //在子线程中调用observeKey()从而使按键事件处理在子线程执行
     events.observeKey();
     events.on("key_down", function(keyCode, events){
-        //音量键关闭脚本
-        if(keyCode == keys.volume_down){
-           // exit();
+        if(keyCode == keys.volume_down){ // 音量下键关闭脚本
+           exit();
         }
     });
 });
+
 events.on("exit", function(){
     toast("脚本已结束");
 });
